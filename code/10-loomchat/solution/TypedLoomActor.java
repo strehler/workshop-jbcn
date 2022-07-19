35589e569d36842f853d610add4eb4f5d89981f6
andreaTP <andrea.peruffo1982@gmail.com>
2022-07-19 10:23:27 +0200

    better loom

--- code/10-loomchat/solution/TypedLoomActor.java
+++ code/10-loomchat/solution/TypedLoomActor.java
@@ -1,24 +1,3 @@
-/*
- *    Inspired by Viktor Klang's minscalaactors.scala
- *    https://gist.github.com/viktorklang/2362563
- *    Copyright 2014 Viktor Klang
- *
- *    Copyright 2021 Edoardo Vacchi
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
- */
-
 //JAVA 19
 //JAVAC_OPTIONS --enable-preview --release 19
 //JAVA_OPTIONS  --enable-preview
@@ -46,25 +25,34 @@ public interface TypedLoomActor {
         // private static ExecutorService executorService = Executors.newFixedThreadPool(5); // clientManager + (2 threads X client)
 
         public <T> Address<T> actorOf(Function<Address<T>, Behavior<T>> initial) {
-            abstract class RunnableAddress<T> implements Address<T>, Runnable { }
-            var addr = new RunnableAddress<T>() {
-                final LinkedBlockingQueue<T> mb = new LinkedBlockingQueue<>();
-                public Address<T> tell(T msg) { mb.offer(msg); return this; }
-                public void run() {
-                    Behavior<T> behavior = initial.apply(this);
-                    while (true) {
-                        try {
-                            T m = mb.take();
-                            behavior = behavior.apply(m).apply(behavior);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                            break;
-                        }
-                    }
-                }
-            };
+            var addr = new RunnableAddress<T>(initial);
             executorService.execute(addr);
             return addr;
+        }
+    }
+
+    class RunnableAddress<T> implements Address<T>, Runnable {
+
+        final Function<Address<T>, Behavior<T>> initial;
+        final LinkedBlockingQueue<T> mb = new LinkedBlockingQueue<>();
+
+        RunnableAddress(Function<Address<T>, Behavior<T>> initial) {
+            this.initial = initial;
+        }
+
+        public Address<T> tell(T msg) { mb.offer(msg); return this; }
+
+        public void run() {
+            Behavior<T> behavior = initial.apply(this);
+            while (true) {
+                try {
+                    T m = mb.take();
+                    behavior = behavior.apply(m).apply(behavior);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
         }
     }
 
